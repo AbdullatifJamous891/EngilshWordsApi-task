2612f6a5773982f5138ba233d4fdeb35f5f20f01
AbdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2018-10-24 08:38:58 +0300

    skilliton app & design

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  word = "";
+
+  app_id = "db81d3b5";
+  app_key = "761c0da04a192ece39097142615cbf79";
+
+
+  fetchData(){
+    
+  }
+   
+}
