4627770a207d6c7f106c9e1c2b770a0fc0e94e34
AbdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2018-10-24 09:24:59 +0300

    api section &add axios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   word = "";
-
+  mean = "";
   app_id = "db81d3b5";
   app_key = "761c0da04a192ece39097142615cbf79";
+  //dict = new Dictionary(this.app_id, this.app_key);
+  //mean = dict.find(this.word,function(error,data){if(error) return console.log(error); console.log(data); });
+  
+  postRequest = {
+    host: "od-api.oxforddictionaries.com",
+    path: "/api/v1/entries/en/" + this.word,
+    port: "443",
+    method: "GET",
+    rejectUnauthorized: false,
+    headers: {
+     'Content-Type': 'text/html',
+     'app_id': this.app_id,
+     'app_key': this.app_key
+    }
+   };
 
 
   fetchData(){
-    
-  }
+  //    axios.get(this.postRequest).then((res)=>{
+         
+  //    }).catch((error)=>{
+  //      console.log(error)
+  //    })
+    }
    
 }
